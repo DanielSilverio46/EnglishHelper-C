5ca06089c33a025e3b77505d9bb88d278c139b5c
Daniel Rocha <danielsr4647@gmail.com>
2025-01-30 11:49:12 -0300

    Created a Simple Window (Linux)

--- src/linux.c
+++ src/linux.c
@@ -1,10 +1,30 @@
 #include <X11/Xlib.h>
 
+#define true 1
+
 int main(void)
 {
-    Display *MainDisplay = XOpenDisplay(NULL);
+    XEvent event;
 
-    XCloseDisplay(MainDisplay);
+    Display *Display = XOpenDisplay(NULL);
+    int screen = DefaultScreen(Display);
+
+    Window MainWindow = XCreateSimpleWindow(Display, RootWindow(Display, screen),
+            0x00, 0x00, 0x190, 0x190, 0x00,
+            BlackPixel(Display, screen), WhitePixel(Display, screen)
+    );
+
+    XSelectInput(Display, MainWindow, ExposureMask | KeyPressMask);
+
+    XMapWindow(Display, MainWindow);
+
+
+    while(true)
+    {
+        XNextEvent(Display, &event);
+    };
+
+    XCloseDisplay(Display);
 
     return 0x00;
 }
