1790ac356f61407d848ec9deb232a4c9faabfd3e
Daniel Rocha <danielsr4647@gmail.com>
2025-02-01 07:59:30 -0300

    how add a input window on main window???

--- src/linux.c
+++ src/linux.c
@@ -1,6 +1,8 @@
 #include <X11/Xlib.h>
+#include <stdio.h>
 
 #define true 1
+
 
 int main(void)
 {
@@ -9,20 +11,28 @@ int main(void)
     Display *Display = XOpenDisplay(NULL);
     int screen = DefaultScreen(Display);
 
+    XSelectInput(Display, RootWindow(Display, screen), ExposureMask | KeyPressMask | FocusChangeMask);
+
     Window MainWindow = XCreateSimpleWindow(Display, RootWindow(Display, screen),
             0x00, 0x00, 0x190, 0x190, 0x00,
             BlackPixel(Display, screen), WhitePixel(Display, screen)
     );
 
-    XSelectInput(Display, MainWindow, ExposureMask | KeyPressMask);
+//    Window Input = XCreateWindow(Display, MainWindow, 0x190/0x02, 0x190/0x02, 0x30, 0x50, 0x00,
+//        0x00, InputOnly, DefaultVisual(Display, screen), 0x00, NULL);
+
 
     XMapWindow(Display, MainWindow);
-
-
-    while(true)
-    {
+ 
+    while(true) {
         XNextEvent(Display, &event);
-    };
+
+        if (event.type == ExposureMask) 
+        {
+        }
+
+        printf("hello");
+    }
 
     XCloseDisplay(Display);
 
